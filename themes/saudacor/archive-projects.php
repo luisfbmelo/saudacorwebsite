50f0adf244a2c6f6e2270c78012c0e57fb83ad04
Luis Melo <luisfbmelo91@gmail.com>
2016-01-19 17:41:36 -0100

    Fixed projects CSS

--- themes/saudacor/archive-projects.php
+++ themes/saudacor/archive-projects.php
@@ -12,31 +12,34 @@ $project_cats = get_terms('project_type',array('hide_empty' => true));
 
 get_header(); ?>
 
+
 	<?php get_template_part('template-parts/content', 'generic-header' ); ?>
-	<?php if(sizeof($project_cats)>0) : ?>
-		<div class="container filters-container">
-			<div class="filter cta primary outlined small" data-filter="all">Todos</div>
-			<?php foreach ($project_cats as $value) { ?>				
-				<div class="filter cta primary outlined small" data-filter=".<?php echo $value->slug; ?>"><?php echo $value->name; ?></div>
-			<?php } ?>
-		</div>
-	<?php endif; ?>
-	<?php if ( have_posts() ) : ?>
-		<div class="container mixitup-container">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php
-					get_template_part( 'template-parts/projects/content', get_post_format() );
-				?>
-			<?php endwhile; ?>
-		</div>
-		<div class="container">
-			<?php wp_pagenavi(); ?>	
-		</div>
+	<div class="archive-container">
+		<?php if(sizeof($project_cats)>0) : ?>
+			<div class="container filters-container">
+				<div class="filter cta primary outlined small" data-filter="all">Todos</div>
+				<?php foreach ($project_cats as $value) { ?>				
+					<div class="filter cta primary outlined small" data-filter=".<?php echo $value->slug; ?>"><?php echo $value->name; ?></div>
+				<?php } ?>
+			</div>
+		<?php endif; ?>
+		<?php if ( have_posts() ) : ?>
+			<div class="container mixitup-container">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php
+						get_template_part( 'template-parts/projects/content', get_post_format() );
+					?>
+				<?php endwhile; ?>
 
-	<?php else : ?>
+			</div>
+			<div class="container">
+				<?php wp_pagenavi(); ?>	
+			</div>
 
-		<?php get_template_part( 'template-parts/content', 'none' ); ?>
+		<?php else : ?>
 
-	<?php endif; ?>
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
+		<?php endif; ?>
+	</div>
 <?php get_footer(); ?>
